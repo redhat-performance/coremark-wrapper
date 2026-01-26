d4a1620973d82b20d0e181c6edf9ad801ec1c6a8
David Valin <dvalin@redhat.com>
2026-01-26 15:02:25 -0500

    Add timestamp to csv file, and switch from : to ,

--- result_schema.py
+++ result_schema.py
@@ -1,6 +1,9 @@
 import pydantic
+import datetime
 
 class CoremarkResults(pydantic.BaseModel):
     iteration: int = pydantic.Field(gt=0)
     threads: int = pydantic.Field(gt=0)
     IterationsPerSec: float = pydantic.Field(gt=0, allow_inf_nan=False)
+    Start_Date: datetime.datetime
+    End_Date: datetime.datetime
